ed6fe3b751b003766381ea0847aec3e452f4b29b
Yuuki Bamba <yuukiBamba@gmail.com>
2019-09-19 12:29:38 +0900

    AssetBundleのテストサンプル作成終了。
    AssetLoadTest.sceneで生成したAssetBundleのインスタンスを生成し、AssetBundleのロードを行っている。

---
+++ AssetBundle/Assets/Scripts/AssetBundle/Editor/BuilderEditor.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+public class BuilderEditor : MonoBehaviour
+{
+    [System.Serializable]
+    struct AssetBundleBuildData
+    {
+        public string assetBundleName;
+        public string[] assetNames;
+    }
+
+    [SerializeField] string outputDir;
+    [SerializeField] AssetBundleBuildData[] datas;
+    //[SerializeField] BuildAssetBundleOptions buildOption;
+    //[SerializeField] BuildTarget buildTarget;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    [MenuItem("Assets/Build AssetBundles")]
+#if false
+    public void Build()
+    {
+        //  ディレクトリ確認(なければ生成)
+        if (!System.IO.Directory.Exists(outputDir))
+        {
+            System.IO.Directory.CreateDirectory(outputDir);
+        }
+
+        //  ビルドデータ生成
+        var builds = new List<AssetBundleBuild>();
+
+        foreach(var it in datas)
+        {
+            var build = new AssetBundleBuild();
+            build.assetBundleName = it.assetBundleName;
+            build.assetNames = it.assetNames;
+            builds.Add(build);
+        }
+
+
+        //BuildPipeline.BuildAssetBundles(outputDir, builds.ToArray(), buildOption, EditorUserBuildSettings.activeBuildTarget);
+        BuildPipeline.BuildAssetBundles(outputDir, builds.ToArray(), 0, EditorUserBuildSettings.activeBuildTarget);
+    }
+#else 
+    static void BuildAllAssetBundles()
+    {
+        //const string c_OutputDir = "Assets/StreamingAssets/AssetBundleResources";
+        const string c_OutputDir = "Assets/AssetBundles";
+        if (!System.IO.Directory.Exists(c_OutputDir))
+        {
+            System.IO.Directory.CreateDirectory(c_OutputDir);
+        }
+        BuildPipeline.BuildAssetBundles(c_OutputDir, BuildAssetBundleOptions.None, EditorUserBuildSettings.activeBuildTarget);
+    }
+#endif
+}
---
+++ AssetBundle/Assets/Scripts/AssetBundle/Sample.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//参考:http://karaagedigital.hatenablog.jp/entry/2016/11/19/170927
+public class Sample : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(DownLoad());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator DownLoad()
+    {
+        //StreamingAssetsを使ったローカルの使用例
+        //"StreamingAssetsパス" + "対象のAssetBundleまでのパス"
+        string bundleURL = Application.streamingAssetsPath + "/AssetBundleResources/testassetbundle";
+        AssetBundleCreateRequest request = AssetBundle.LoadFromFileAsync(bundleURL);
+        while(!request.isDone)
+        {
+            yield return null;
+        }
+        AssetBundle bundle = request.assetBundle;
+        AssetBundleRequest asset = bundle.LoadAssetAsync<GameObject>("Canvas");
+        //  ここまで!
+
+        //下記 生成しているだけ
+        GameObject obj = (GameObject)Instantiate(asset.asset);
+    }
+}
